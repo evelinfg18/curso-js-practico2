c1523642f7a17536b889d512bf545fa47b30a070
evelinfg18 <evelinfg18@hotmail.com>
2022-09-19 01:18:33 -0500

    manejo de eventos en forms

--- eventos.js
+++ eventos.js
@@ -3,10 +3,10 @@ const input1=document.querySelector('#calculo1');
 const input2=document.querySelector('#calculo2');
 const btn=document.querySelector('#btnCalcular');
 const pResult=document.querySelector('#resultado');
-
+btn.addEventListener('click',btnOnClick)
 //escuchar los eventos desde javascript
 function btnOnClick(){
-    const sumaInputs= input1.value + input2.value;
+    const sumaInputs= Number(input1.value) + Number(input2.value);
     pResult.innerText="Resultado: " + sumaInputs;
     //console.log('Escuchando el evento btnOcnClick al pulsar el botón');
 };
